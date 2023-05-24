b5347c23f58789821e45abdbeb7d7c0210ed2ef1
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-05-24 10:43:28 +0300

    Update electron path to index file

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'path';
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
   // Create the browser window.
@@ -12,39 +12,7 @@ function createWindow() {
   });
 
   // Load the Ionic app.
-  mainWindow.loadURL(`file://${path.join(__dirname, 'www/index.html')}`);
-
-  // Open the DevTools (optional).
-  // mainWindow.webContents.openDevTools();
-}
-
-// Called when Electron has finished initialization.
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
-});
-
-
-function createWindow() {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-
-  // Load the Ionic app.
-  mainWindow.loadURL(`file://${path.join(__dirname, 'www/index.html')}`);
+  mainWindow.loadURL(`file://${path.join(__dirname, './dist/index.html')}`);
 
   // Open the DevTools (optional).
   // mainWindow.webContents.openDevTools();
