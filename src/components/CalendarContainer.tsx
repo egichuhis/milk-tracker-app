16454abdf15d0b9944cbf019380b85ae46d2b1f7
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-05-29 19:10:34 +0300

    Add CalendarContainer

---
+++ src/components/CalendarContainer.tsx
@@ -0,0 +1,15 @@
+import './CalendarContainer.css';
+
+interface ContainerProps {
+  name: string;
+}
+
+const CalendarContainer: React.FC<ContainerProps> = ({ name }) => {
+  return (
+    <div className="container">
+     {/* Add Calendar */}
+    </div>
+  );
+};
+
+export default CalendarContainer;
